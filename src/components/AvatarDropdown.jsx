11c94cf3abd9c2fbe84b519cc4e94c0d86ee07cd
GeorgeA25 <GeorgeA25@users.noreply.github.com.>
2025-07-10 11:12:19 +0100

    Add props to AvatarDropdwon and assign onClick handlers

--- src/components/AvatarDropdown.jsx
+++ src/components/AvatarDropdown.jsx
@@ -1,17 +1,16 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import avatarImg from './images/user-avatar-photo.webp'; // caminho igual ao que já usa
+import Dropdown from "react-bootstrap/Dropdown";
+import avatarImg from "./images/user-avatar-photo.webp"; 
 
-
-function AvatarDropdown() {
+function AvatarDropdown({ user, onSignOut, onDelete }) {
   return (
     <Dropdown align="end">
       <Dropdown.Toggle variant="link" className="avatar-button p-0">
         <img src={avatarImg} className="avatar-img" alt="User avatar" />
       </Dropdown.Toggle>
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Your profile</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Log out</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Delete account</Dropdown.Item>
+        <Dropdown.ItemText>{user?.isAnonymous ? "Guest" : user?.email}</Dropdown.ItemText>
+        <Dropdown.Item onClick={onSignOut}>Log out</Dropdown.Item>
+        <Dropdown.Item onClick={onDelete}>Delete account</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
