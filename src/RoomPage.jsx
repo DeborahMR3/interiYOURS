54f3437676c3dbf38dd195adb93dba9752f1d391
j-cocker <jncocker@icloud.com>
2025-07-15 11:16:23 +0100

    Add basic UI control buttons to scene ready to add functionality

--- src/RoomPage.jsx
+++ src/RoomPage.jsx
@@ -4,10 +4,12 @@ import Sidebar from "./components/Sidebar";
 import "./components/styling/RoomPage.css";
 import { useParams } from "react-router-dom";
 import { getRoomById } from "./firebase/firebaseStore";
+import { ControlButtons } from "./components/ControlButtons";
 
 const RoomPage = () => {
   const [currentLayout, setCurrentLayout] = useState([]);
   const [isItemAdded, setIsItemAdded] = useState(false);
+  const [isRotating, setIsRotating] = useState(false);
   const [roomData, setRoomData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -59,6 +61,7 @@ const RoomPage = () => {
         isItemAdded={isItemAdded}
         setIsItemAdded={setIsItemAdded}
       />
+      <ControlButtons isRotating={isRotating} setIsRotating={setIsRotating} />
     </div>
   );
 };
