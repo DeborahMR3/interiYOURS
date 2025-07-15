8603fedaae4dc8f783fb34a10c02588345dee76c
j-cocker <jncocker@icloud.com>
2025-07-15 20:58:04 +0100

    Add icons for popup, share and home functionality

--- src/components/ControlButtons.jsx
+++ src/components/ControlButtons.jsx
@@ -1,9 +1,13 @@
 import { FaRegSave } from "react-icons/fa";
 import { FaUndo } from "react-icons/fa";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { IoHomeOutline } from "react-icons/io5";
+import { IoShareSocialOutline } from "react-icons/io5";
 //import { BsArrowsMove } from "react-icons/bs";
 import { FaArrowsAlt } from "react-icons/fa";
 import { FaArrowsSpin } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 import "./styling/ControlButtons.css";
 
 import AvatarDropdown from "./AvatarDropdown";
@@ -22,6 +26,12 @@ export const ControlButtons = ({
       <AvatarDropdown className="avatar-button" align="end" />
       <button className="control-button" onClick={handleSavedPositions}>
         <FaRegSave />
+      </button>
+      <button className="control-button">
+        <IoHomeOutline />
+      </button>
+      <button className="control-button">
+        <IoShareSocialOutline />
       </button>
       <button className="control-button">
         <FaUndo />
