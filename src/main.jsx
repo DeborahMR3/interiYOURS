a1985411eaebccabf0a0c15eab4395f058817bc8
j-cocker <jncocker@icloud.com>
2025-07-15 14:57:02 +0100

    Patch: corrected import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css"; //
 
-import "./components/styling/global.css";
+import "./components/styling/Global.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
